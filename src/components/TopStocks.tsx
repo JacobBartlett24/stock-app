ea4243a8cb449492908434ba8261cc9da3946e9c
JacobBartlett24 <jacobart@ttu.edu>
2023-01-25 11:11:01 -0600

    Initial commit

---
+++ src/components/TopStocks.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TopStocks = () => {
+  return (
+    <div>
+      <h1>Top Stocks</h1>
+    </div>
+  );
+}
+
+export default TopStocks;
---
+++ src/components/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TopStocks from "./TopStocks";
+import Stocks from "./Stocks";
+
+const Main = () => {
+  return (
+    <div>
+      <TopStocks />
+      <Stocks />
+    </div>
+  );
+}
+
+export default Main;
